400a2ce9b7ab9a8a8d4baf9bdf3d6802d98026ce
kronos1046T <maksim.tereshchuk@helsinki.fi>
2026-05-18 11:31:28 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,3 +2,9 @@ CREATE TABLE visits (
     id INTEGER PRIMARY KEY,
     visited_at TEXT
 );
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
