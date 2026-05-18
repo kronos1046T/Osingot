cc61c071741c11479ff02a71edf1a25c3b804a91
kronos1046T <maksim.tereshchuk@helsinki.fi>
2026-05-18 11:22:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
 app = Flask(__name__)
